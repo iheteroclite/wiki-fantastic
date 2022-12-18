e1d78a4cf1eb61f14d53fcb14428df6c3743d962
iheteroclite <118612115+iheteroclite@users.noreply.github.com>
2022-12-18 00:24:12 +0000

    urls now prefix wiki, 404 for unknown topic

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -13,13 +13,10 @@ def entry(request, title):
     match = [i for i in util.list_entries() if (i.lower() == title.lower())]
     if match:
         return render(request, 'encyclopedia/entry.html', {
-            'title': title,
-            'text': util.get_entry(match[0])
+            'title': match[0],
+            'text': markdown(util.get_entry(match[0]))
         })
-    # TODO: test if more than one match?
-
-    # TODO: make a 404 page
-    return render(request, 'encyclopedia/entry.html', {
-        'title': title,
-        'text': f'No Entry for {title}'
+    # TODO: test if more than one match - display options
+    return render(request, 'encyclopedia/404.html', {
+        'title': title
     })
